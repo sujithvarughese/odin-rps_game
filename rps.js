2b44d9931cf52438665bd6aaa3058a80e01b7c39
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2022-11-14 20:14:26 -0500

    Created game() function to keep track of score and call all other functions

--- rps.js
+++ rps.js
@@ -5,37 +5,45 @@ function getComputersChoice() {
     const playOptions = ['rock', 'paper', 'scissors'];
     return playOptions[Math.floor(Math.random() * 3)];
 }
-
+const compChoice = () =>
 // get players play
+
 function getPlayersChoice() {
     let playerSelection = '';
     let moveValidated = false;
     do {
-        playerSelection = prompt('Enter Move:');
+        playerSelection = prompt('Choose rock, paper, or scissors:');
         playerSelection.toLowerCase();
+        if (playerSelection === 'y') {
+            break;
+        }
         if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'){
             moveValidated = true;
         }
+        else {
+            console.log('Want to quit? Type "Y" to quit, or please enter a valid move to play!')
+        }
     } while (!moveValidated);
+    return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `Both players chose ${playerSelection}! Tie game!`
-    }
     let playerWin = false;
+    if (playerSelection === computerSelection) {
+        return `Both players chose ${playerSelection}! Tie game!`;
+    }
     else {
         if (playerSelection === 'rock') {
             if (computerSelection === 'scissors') {
                 playerWin = true;
             }
         }
-        if (playerSelection === 'paper') {
+        else if (playerSelection === 'paper') {
             if (computerSelection === 'rock') {
                 playerWin = true;
             }
         }
-        if (playerSelection === 'scissors') {
+        else if (playerSelection === 'scissors') {
             if (computerSelection === 'paper') {
                 playerWin = true;
             }
@@ -45,6 +53,8 @@ function playRound(playerSelection, computerSelection) {
         `${computerSelection} beats ${playerSelection}. Sorry, you lose.`;
 }
 
-
-
+function game() {
+    this.playerScore = 0;
+    this.computerScore = 0;
+}
 console.log(getComputersChoice())
